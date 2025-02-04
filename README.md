4c3711ca6d11d7a3f1a51bcdac0c3cda7ae93fdc
varunch29 <varunreddynani29@gmail.com>
2025-02-05 00:31:38 +0530

    Updated footer.jsx

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+# myportfolio
+=======
+>>>>>>> 501eecf (first commit)
 <p align="center" width="100%">
     <img height="100" src="https://github.com/said7388/developer-portfolio/assets/77630868/c0064908-cd5f-4751-a77c-eba90a62b55c">
 </p>
@@ -273,3 +279,7 @@ npm install -g next
 This installs Next.js globally
 
 then do the usual `npm run dev`
+<<<<<<< HEAD
+=======
+>>>>>>> 14cdf6e (first commit)
+>>>>>>> 501eecf (first commit)
--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Varun Reddy</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/varun-reddy-378a4a2a6/" className="text-[#16f2b3]">Varun Reddy</Link>
           </p>
           <div className="flex items-center gap-5">
             {/* <Link
