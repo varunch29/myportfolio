14cdf6e2c1397c399d6994cbab7d97a2fb79eb58
varunch29 <varunreddynani29@gmail.com>
2025-02-04 23:56:57 +0530

    first commit

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'varunreddynani29@gmail.com',
+    phone: '+91 6302295302',
+    address: '1-8/1, chaitanya colony, Huzurnagar, Suryapet, Telangana',
+    github: 'https://github.com/said7388',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "said7388"
+}
---
+++ utils/data/projects-data.js
@@ -0,0 +1,53 @@
+export const projectsData = [
+    {
+        id: 1,
+        name: 'BE FIT',
+        description: "Be Fit is a term that encompasses the idea of maintaining a healthy and active lifestyle through a combination of regular exercise, balanced nutrition, and mental well-being. It promotes physical fitness, strength, endurance, flexibility, and overall health.",
+        tools: ['Express', 'MongoDB', 'OpenAI API', 'AWS SES', 'AWS S3', 'Node Mailer', 'Joi', 'Puppeteer', 'EC2', 'PM2', 'Nginx'],
+        role: 'BFull Stack Developer',
+        code: '',
+        demo: '',
+    },
+    {
+        id: 2,
+        name: 'Doctor Appointment App',
+        description: 'A doctor appointment is a scheduled meeting between a patient and a healthcare professional for medical consultation, diagnosis, treatment, or follow-up care. These appointments can be in-person at a clinic/hospital or virtual through telemedicine platforms.',
+        tools: ['NextJS', 'Tailwind CSS', "Google Maps", "NestJS", "TypeScript", "MySQL", "AWS S3", "Sun-Editor", "Gmail Passkey"],
+        role: 'Full Stack Developer',
+        code: '',
+        demo: '',
+    }
+    // },
+    // {
+    //     id: 3,
+    //     name: 'AI Powered Real Estate',
+    //     description: 'My team built an AI-based real estate app using Replicate API and OpenAI. We used Express, Typescript, OpenAI, Replicate, Stripe, and Mongoose to develop the API. We utilized NextJS, Formik, TailwindCSS, and other npm libraries for the UI. We have trained multiple AI assistants using the latest GPT model and integrated Replicate API for image processing. We added role-based auth, subscription plans, Cron job scheduling, and payment integration with Stripe.',
+    //     tools: ['React', 'Bootstrap', 'SCSS', 'Stripe', 'Express', 'TypeScript', 'MongoDB', 'Azure Blob', 'OpenAI API', 'Replicate AI', 'Cronjob', 'JWT'],
+    //     code: '',
+    //     role: 'Full Stack Developer',
+    //     demo: '',
+    // },
+    // {
+    //     id: 4,
+    //     name: 'Newsroom Management',
+    //     description: "My team and I developed a newspaper management dashboard application called Newsroom Management. As a front-end developer, I worked on creating the dashboard using NextJS, Material UI, Redux, Calendar, and other necessary npm libraries. We used React Redux to manage the application's state and React-hook-form and Sun Editor to handle forms.",
+    //     tools: ['NextJS', 'Material UI', 'Redux', 'Sun Editor', "Calendar"],
+    //     code: '',
+    //     demo: '',
+    //     role: 'Full Stack Developer',
+    // }
+];
+
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+// },
