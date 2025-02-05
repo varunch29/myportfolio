407d39a3dc59f96330ace0ac80a572928bf13c5e
varunch29 <varunreddynani29@gmail.com>
2025-02-05 07:17:47 +0530

    Add missing components

--- app/blog/page.js
+++ app/blog/page.js
@@ -1,30 +1,32 @@
 import { personalData } from "@/utils/data/personal-data";
-import AboutSection from "./components/homepage/about";
-import ContactSection from "./components/homepage/contact";
-import Education from "./components/homepage/education";
-import HeroSection from "./components/homepage/hero-section";
-import Projects from "./components/homepage/projects";
-import Skills from "./components/homepage/skills";
+import AboutSection from "@/components/homepage/about";
+import ContactSection from "@/components/homepage/contact";
+import Education from "@/components/homepage/education";
+import HeroSection from "@/components/homepage/hero-section";
+import Projects from "@/components/homepage/projects";
+import Skills from "@/components/homepage/skills";
 
 async function getData() {
-  const res = await fetch(https://dev.to/api/articles?username=${personalData.devUsername})
+  try {
+    const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await res.json();
+    return data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
   }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
+}
 
 export default async function Home() {
   const blogs = await getData();
 
   return (
-    <div suppressHydrationWarning >
+    <div suppressHydrationWarning>
       <HeroSection />
       <AboutSection />
       <Skills />
@@ -32,5 +34,5 @@ export default async function Home() {
       <Education />
       <ContactSection />
     </div>
-  )
-};
+  );
+}
