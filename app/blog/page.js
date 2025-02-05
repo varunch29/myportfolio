25e170e674454b423f89edd2254003a5783924c1
varunch29 <varunreddynani29@gmail.com>
2025-02-05 07:29:56 +0530

    Renamed hero-section and projects correctly

--- app/blog/page.js
+++ app/blog/page.js
@@ -2,9 +2,10 @@ import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "@/components/homepage/about";
 import ContactSection from "@/components/homepage/contact";
 import Education from "@/components/homepage/education";
-import HeroSection from "@/components/homepage/hero-section";
-import Projects from "@/components/homepage/projects";
+import HeroSection from "@/components/homepage/HeroSection";
+import Projects from "@/components/homepage/Projects";
 import Skills from "@/components/homepage/skills";
+
 
 async function getData() {
   try {
