81ff89a5dcc11f1a76e8516a3688bcd280459f28
varunch29 <varunreddynani29@gmail.com>
2025-02-05 06:37:20 +0530

    updated page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,7 @@
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
-import Blog from "./components/homepage/blog";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
-import Experience from "./components/homepage/experience";
 import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
@@ -29,11 +27,9 @@ export default async function Home() {
     <div suppressHydrationWarning >
       <HeroSection />
       <AboutSection />
-      {/* <Experience /> */}
       <Skills />
       <Projects />
       <Education />
-      {/* <Blog blogs={blogs} /> */}
       <ContactSection />
     </div>
   )
